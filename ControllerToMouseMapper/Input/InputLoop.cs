a432ea03d3b59306984f4ab7ea04ead65b105f32
Adérito Silva <pespego@outlook.com>
2024-09-05 13:53:48 +0100

    Adicionar arquivos de projeto.

---
+++ ControllerToMouseMapper/Input/InputLoop.cs
@@ -0,0 +1,106 @@
+﻿using XInputium;
+
+namespace ControllerToMouseMapper.Input;
+
+/// <summary>
+/// Implements a thread safe input loop.
+/// </summary>
+/// <remarks>
+/// <see cref="InputLoop"/> iterates at a specified frequency and calls
+/// a provided callback <see cref="Action"/> on every iteration. This
+/// callback is called in a thread safe manner -- it is never called
+/// concurrently. However, it may be called on different threads each
+/// time. The callback is run in a thread from the thread pool.
+/// </remarks>
+public sealed class InputLoop : IDisposable, IAsyncDisposable
+{
+
+
+    private readonly Action _onUpdate;
+    private readonly InputLoopWatch _watch;
+    private readonly Timer _timer;
+    private bool _disposed, _disposing;
+    private int _isUpdating;
+
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InputLoop"/> class,
+    /// that uses the specified callback for iterations and the specified
+    /// number of iterations per second.
+    /// </summary>
+    /// <param name="onUpdate"><see cref="Action"/> that is invoked
+    /// on every iteration of the input loop.</param>
+    /// <param name="frequency">Optional. Number of desired iterations
+    /// per second. The default is 60.</param>
+    /// <param name="watch">Optional. The <see cref="InputLoopWatch"/> instance
+    /// used to measure time, or <see langword="null"/> to use the default
+    /// implementation. The default value is <see langword="null"/>.</param>
+    /// <exception cref="ArgumentNullException">
+    /// <paramref name="onUpdate"/> is <see langword="null"/>.</exception>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// <paramref name="frequency"/> is equal to or less than 0.</exception>
+    public InputLoop(Action onUpdate, int frequency = 60, InputLoopWatch? watch = null)
+    {
+        ArgumentNullException.ThrowIfNull(onUpdate);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(frequency);
+
+        _onUpdate = onUpdate;
+        _watch = watch ?? InputLoopWatch.GetDefault();
+        _timer = new(OnTick, null, 0, 1000 / frequency);
+    }
+
+
+    /// <summary>
+    /// Gets the time elapsed between the two most recent update operations.
+    /// This allows you to measure the most recent iteration time.
+    /// </summary>
+    public TimeSpan IterationTime { get; private set; }
+
+
+    /// <inheritdoc/>
+    public void Dispose()
+    {
+        if (_disposed || _disposing)
+            return;
+        _disposing = true;
+
+        _timer.Dispose();
+
+        _disposed = true;
+        _disposing = false;
+    }
+
+
+    /// <inheritdoc/>
+    public async ValueTask DisposeAsync()
+    {
+        if (_disposed)
+            return;
+        _disposing = true;
+
+        await _timer.DisposeAsync();
+
+        _disposed = true;
+        _disposing = false;
+    }
+
+
+    private void OnTick(object? state)
+    {
+        if (Interlocked.Exchange(ref _isUpdating, 1) == 0)
+        {
+            try
+            {
+                IterationTime = _watch.GetTime();
+                _onUpdate();
+            }
+            finally
+            {
+                Interlocked.Exchange(ref _isUpdating, 0);
+            }
+        }
+
+    }
+
+
+}
---
+++ ControllerToMouseMapper/Program.cs
@@ -0,0 +1,100 @@
+﻿using ControllerToMouseMapper.Input;
+using XInputium.ModifierFunctions;
+using XInputium.XInput;
+
+// For this sample project, we are using our own simple implementation of mouse and
+// keyboard interop. But for more complete and capable keyboard and mouse simulation
+// features, check out: https://github.com/MediatedCommunications/WindowsInput/
+
+// Show some initial text in the console.
+Console.WriteLine("This application maps the controller to the mouse. " +
+    "This is just a demo application, with minimal functionality.");
+Console.WriteLine();
+Console.WriteLine("--Left joystick: mouse movement");
+Console.WriteLine("--A: Left mouse button");
+Console.WriteLine("--X: Right mouse button");
+Console.WriteLine("--LS: Middle mouse button");
+Console.WriteLine("--B: Escape key");
+Console.WriteLine("--LB: Shift+Tab key");
+Console.WriteLine("--LR: Tab key");
+Console.WriteLine("--DPad: Arrow keys");
+
+// =========================================================
+
+
+XGamepad gamepad = new();
+
+var mouseJoystick = gamepad.LeftJoystick;
+var leftMouseButton = gamepad.Buttons.A;
+var rightMouseButton = gamepad.Buttons.X;
+var middleMouseButton = gamepad.Buttons.LS;
+
+// Configure the mouse joystick.
+mouseJoystick.InnerDeadZone = 0.2f;  // Let's use a circular dead zone.
+mouseJoystick.RadiusModifierFunction = NonLinearFunctions.QuadraticEaseIn;  // Make the joystick more precise near the center.
+
+// Map the joystick to the mouse.
+double moveSpeed = 0.01;  // Move speed, in screen ratio per second.
+mouseJoystick.Updated += (sender, e) =>
+{
+    // Update the mouse on every input loop update. If the joystick is at rest position,
+    // we don't need to move the mouse. Notice we are not using the joystick move event,
+    // because we want the mouse to keep moving while the joystick is being held (pushed).
+    if (mouseJoystick.IsPushed)
+    {
+        Mouse.MoveBy(
+            mouseJoystick.X * moveSpeed * gamepad.FrameTime.TotalSeconds,
+            -mouseJoystick.Y * moveSpeed * gamepad.FrameTime.TotalSeconds);
+    }
+};
+
+// Map controller buttons to the mouse buttons.
+leftMouseButton.Pressed += (sender, e) => Mouse.PressButton(MouseButton.Left);
+leftMouseButton.Released += (sender, e) => Mouse.ReleaseButton(MouseButton.Left);
+rightMouseButton.Pressed += (sender, e) => Mouse.PressButton(MouseButton.Right);
+rightMouseButton.Released += (sender, e) => Mouse.ReleaseButton(MouseButton.Right);
+middleMouseButton.Pressed += (sender, e) => Mouse.PressButton(MouseButton.Middle);
+middleMouseButton.Released += (sender, e) => Mouse.ReleaseButton(MouseButton.Middle);
+
+// Map controller buttons to keyboard keys.
+TimeSpan buttonRepeatDelay = TimeSpan.FromMilliseconds(350);
+TimeSpan buttonRepeatInterval = TimeSpan.FromMilliseconds(100);
+gamepad.RegisterButtonRepeatEvent(XButtons.DPadUp, buttonRepeatDelay, buttonRepeatInterval,
+    (_, e) => Keyboard.TapKey(KeyboardVirtualKey.Up));
+gamepad.RegisterButtonRepeatEvent(XButtons.DPadDown, buttonRepeatDelay, buttonRepeatInterval,
+    (_, e) => Keyboard.TapKey(KeyboardVirtualKey.Down));
+gamepad.RegisterButtonRepeatEvent(XButtons.DPadLeft, buttonRepeatDelay, buttonRepeatInterval,
+    (_, e) => Keyboard.TapKey(KeyboardVirtualKey.Left));
+gamepad.RegisterButtonRepeatEvent(XButtons.DPadRight, buttonRepeatDelay, buttonRepeatInterval,
+    (_, e) => Keyboard.TapKey(KeyboardVirtualKey.Right));
+
+gamepad.RegisterButtonRepeatEvent(XButtons.LB, buttonRepeatDelay, buttonRepeatInterval,
+    (_, e) =>
+    {
+        Keyboard.PressKey(KeyboardVirtualKey.Shift);
+        Keyboard.TapKey(KeyboardVirtualKey.Tab);
+        Keyboard.ReleaseKey(KeyboardVirtualKey.Shift);
+    });
+gamepad.RegisterButtonRepeatEvent(XButtons.RB, buttonRepeatDelay, buttonRepeatInterval,
+    (_, e) => Keyboard.TapKey(KeyboardVirtualKey.Tab));
+
+gamepad.Buttons.B.Pressed += (sender, e) => Keyboard.TapKey(KeyboardVirtualKey.Escape);
+
+
+// Start the input loop. Note that any changes you may want to make on the gamepad must
+// always be done within the input loop update callback, to ensure thread safety.
+using InputLoop inputLoop = new(() =>
+{
+    gamepad.Update();
+    if (!gamepad.IsConnected)
+    {
+        gamepad.Device = XInputDevice.GetFirstConnectedDevice();
+    }
+});
+
+// =====================================
+
+// Wait for the user to press any key on the console, before exiting.
+Console.WriteLine();
+Console.WriteLine("Press any key to terminate the application.");
+Console.ReadKey();
